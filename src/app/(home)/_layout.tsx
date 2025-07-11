27513e115722f154cc96d418f3a7347fdd11e7ac
yuyujijin <eugenevalty@gmail.com>
2025-07-11 14:54:14 +0200

    Added temporary image for `(home)`

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,14 +1,31 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { ImageBackground, StyleSheet } from "react-native";
+
+const image = require("../../../assets/images/home.png");
 
 export default function TabLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
+    <ImageBackground
+      source={image}
+      style={styles.backgroundImage}
+      resizeMode={"cover"}
     >
-      <Stack.Screen name="index" />
-    </Stack>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+});
