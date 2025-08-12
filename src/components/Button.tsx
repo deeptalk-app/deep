3c8cffee953a313ee2a3ab41687048f89d938358
yuyujijin <eugenevalty@gmail.com>
2025-08-12 21:31:45 +0200

    Made `Button` component more flexible

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,13 @@
 import { TouchableHighlight } from "react-native";
 import { TouchableHighlightProps } from "react-native-gesture-handler";
 
-export function Button(props: TouchableHighlightProps) {
+export function Button({ className, ...props }: TouchableHighlightProps) {
   return (
     <TouchableHighlight
       className={[
         "flex-1 items-center p-5 rounded-[1000px] border border-white opacity-100 bg-black/[.4]",
         props.disabled && "opacity-40",
+        className,
       ].join(" ")}
       {...props}
     >
